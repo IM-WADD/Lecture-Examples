d862fe184ce029a4847188503b1ec63b62d58fb2
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-03-06 15:01:43 +0000

    completed counter example w4l2

---
+++ Lectures/W4Lecture2/counter/counters.js
@@ -0,0 +1,19 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+let count1 = 0;
+let count2 = 0;
+
+
+function addClick(event) {
+    console.log(event.target.id, "button clicked at", event.timestamp);
+    if (event.target.id === "btn1") {
+        count1++;
+        document.getElementById("count1").innerText = count1;
+    } else {
+        count2++;
+        document.getElementById("count2").innerText = count2;
+    }
+}
+
+btn1.addEventListener("click", addClick);
+btn2.addEventListener("click", addClick);
