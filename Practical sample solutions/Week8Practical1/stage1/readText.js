5edd98747f16d91333532ada83ccf4d1d54a1d0f
Abi Evans <50892754+ace01603@users.noreply.github.com>
2026-02-11 16:40:09 +0000

    removed old folders

--- Practical sample solutions/Week8Practical1/stage1/readText.js
+++
@@ -1,7 +0,0 @@
-const storedText = sessionStorage.getItem("W8P1");
-if (storedText !== null) {
-    const showElements = document.getElementsByClassName("show-text");
-    for (const elem of showElements) {
-        elem.innerText = storedText;
-    }
-}
