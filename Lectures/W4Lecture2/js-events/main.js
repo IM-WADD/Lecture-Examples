0d9cf2a3db90be9d2276d99497938daa1397e250
Abi Evans <50892754+ace01603@users.noreply.github.com>
2026-03-05 10:03:03 +0000

    w4l2 complete

--- Lectures/W4Lecture2/js-events/main.js
+++ Lectures/W4Lecture2/js-events/main.js
@@ -1,3 +1,22 @@
 // Step 1. Select the elements you want to respond to events
 // Step 2. Write event handlers - functions that will run when the event happens
 // Step 3. Attach the event handlers to the elements
+
+// Step 1.
+const button = document.getElementById("myButton");
+
+// Step 2.
+function changeCircleColour() {
+    console.log("change me");
+    const circle = document.getElementById("circle");
+    circle.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+}
+
+function changeBackgroundColour(event) {
+    console.log(event.target);
+    document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+}
+
+// Step 3.
+button.addEventListener("click", changeCircleColour);
+document.addEventListener("click", changeBackgroundColour);
